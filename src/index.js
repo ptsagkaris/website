2aa173e8cc4e072e83d5bbc5fc4699710feea0ae
Panagiotis Tsagkaris <tsagkarisp@gmail.com>
2021-09-07 17:42:14 +0100

    Added auth and amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import Amplify, { Auth } from 'aws-amplify'
+import awsconfig from './aws-exports'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
 
 ReactDOM.render(
   <React.StrictMode>
